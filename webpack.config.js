4833662617ba5203aedbfd72032cfcaf4beb83b3
dankoknad <dankomilutinovic@gmail.com>
2017-04-14 17:17:22 +0200

    02 Webpack 2 - HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		filename: './dist/app.bundle.js'		
-	}
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.bundle.js'		
+	},
+	plugins: [new HtmlWebpackPlugin({
+		title: 'Webpack 2 is cool',
+    template: './src/index.ejs',
+		hash: true,
+		minify: {
+			collapseWhitespace: false
+		}
+	})]
 }
